c4d3f75b9e8c17ce2e4f025bbf8d03d7c0deb64f
Wonho ha <haho013039@gmail.com>
2018-11-22 20:01:35 +0900

    인자값 넣기

--- 1day/1day/one.cpp
+++ 1day/1day/one.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
-void print() {
-	printf("Las Wonho");
+void print(char* string) {
+	printf(string);
 }
 const char* name= "wonho";
 _declspec(naked) void main(void) {
 	_asm {
+		push 0;
+		popf
+		
+		push name;
+
 		call print;
 
 		stay:

e141ae420818e549a1e0f235fee3d1d84a6cd2a5
Wonho ha <haho013039@gmail.com>
2018-11-29 12:35:09 +0900

    함수에서 사용

--- 2day/2day/main.cpp
+++ 2day/2day/main.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include<iostream>
 using namespace std;
+int Add(int a, int b) {
+	__asm {
+		mov eax, a;
+		add eax, b;
+	}
+	//return eax
+}
 void main() {
-	const int t=2;
-	int result = 0;
-	_asm{
-		mov eax, t;
-		add eax, 10;
-		add result, eax;
-	};
-	cout << result;
+	int t = Add(1, 2);
+	cout << t;
 	for (;;);
 }
